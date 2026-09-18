19e5ff9afdd6131f23157c2b2a256ec3af41a144
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-18 11:37:07 +0400

    model: add Model base

---
+++ include/gungnir/model/model.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <gungnir/model/field.hpp>
+#include <gungnir/model/foreign_key.hpp>
+#include <gungnir/model/primary_key.hpp>
+#include <gungnir/model/relation.hpp>
+#include <gungnir/model/table.hpp>
+
+namespace gungnir {
+
+template <typename Derived>
+class Model {
+public:
+    using model_type = Derived;
+
+protected:
+    Model() = default;
+    ~Model() = default;
+};
+
+} // namespace gungnir
