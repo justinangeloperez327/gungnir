102fe76762b3cb5ea655c67e7f3136d0fad05a2c
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-18 10:41:40 +0400

    http: add response abstraction

---
+++ include/gungnir/http/response.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+#include <unordered_map>
+
+namespace gungnir::http {
+
+class Response {
+public:
+    using Headers = std::unordered_map<std::string, std::string>;
+
+    Response(int status = 200, std::string body = {});
+
+    [[nodiscard]] int status() const noexcept;
+    [[nodiscard]] std::string_view body() const noexcept;
+
+    Response& status(int value) noexcept;
+    Response& body(std::string value);
+    Response& header(std::string name, std::string value);
+
+    [[nodiscard]] std::string_view header(std::string_view name) const noexcept;
+    [[nodiscard]] const Headers& headers() const noexcept;
+
+    [[nodiscard]] static Response text(std::string body, int status = 200);
+    [[nodiscard]] static Response not_found();
+
+private:
+    int status_;
+    std::string body_;
+    Headers headers_;
+};
+
+} // namespace gungnir::http
