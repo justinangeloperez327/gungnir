c61dd00046af3306894df18cd3095b9de6a4f841
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-22 09:28:14 +0400

    fix(controller): own lazy coroutine action results

--- include/gungnir/controller/action.hpp
+++ include/gungnir/controller/action.hpp
@@ -15,17 +15,15 @@ template <typename>
 inline constexpr bool unsupported_action_result = false;
 
 template <typename Result>
-[[nodiscard]] Task<http::Response> normalize(Result&& result) {
+[[nodiscard]] Task<http::Response> normalize(Result result) {
     using Value = std::remove_cvref_t<Result>;
 
     if constexpr (std::same_as<Value, http::Response>) {
-        co_return std::forward<Result>(result);
+        co_return std::move(result);
     } else if constexpr (std::same_as<Value, Task<http::Response>>) {
-        co_return co_await std::forward<Result>(result);
+        co_return co_await std::move(result);
     } else if constexpr (std::same_as<Value, std::string>) {
-        co_return http::Response::text(
-            std::forward<Result>(result)
-        );
+        co_return http::Response::text(std::move(result));
     } else if constexpr (std::same_as<Value, std::string_view>) {
         co_return http::Response::text(std::string{result});
     } else {
