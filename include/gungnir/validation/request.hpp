5990f00c84d22bd7cb4db41a1032bb01c5e84101
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-22 08:04:42 +0400

    feat(validation): add validated request abstraction

---
+++ include/gungnir/validation/request.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <utility>
+#include <gungnir/http/request.hpp>
+#include <gungnir/validation/rules.hpp>
+#include <gungnir/validation/validator.hpp>
+
+namespace gungnir::validation {
+
+class ValidatedRequest {
+public:
+    explicit ValidatedRequest(const http::Request& request) : request_(&request) {}
+    virtual ~ValidatedRequest() = default;
+
+    [[nodiscard]] Input validated() const {
+        return Validator::validate(request_->all(), rules());
+    }
+
+    [[nodiscard]] const http::Request& request() const noexcept { return *request_; }
+
+protected:
+    [[nodiscard]] virtual Rules rules() const = 0;
+
+private:
+    const http::Request* request_;
+};
+
+} // namespace gungnir::validation
+
+namespace gungnir {
+using ValidatedRequest = validation::ValidatedRequest;
+}
