3fddb4ef666f2407b4b6cc9de57ac2f648707cd6
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-22 07:48:57 +0400

    feat(http): add HTTP runtime foundations

---
+++ include/gungnir/http/runtime.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <chrono>
+#include <cstddef>
+
+namespace gungnir::http {
+
+struct RuntimeOptions {
+    std::size_t max_request_bytes{1024U * 1024U};
+    std::size_t max_header_bytes{64U * 1024U};
+    std::size_t max_connections{4096};
+    std::size_t max_requests_per_connection{100};
+    std::chrono::milliseconds read_timeout{30000};
+    std::chrono::milliseconds write_timeout{30000};
+    std::chrono::milliseconds idle_timeout{15000};
+    bool keep_alive{true};
+};
+
+enum class HttpVersion { http_1_0, http_1_1, http_2 };
+enum class ConnectionDirective { keep_alive, close };
+
+struct ConnectionPolicy {
+    HttpVersion version{HttpVersion::http_1_1};
+    ConnectionDirective directive{ConnectionDirective::keep_alive};
+    std::size_t requests_served{0};
+};
+
+} // namespace gungnir::http
