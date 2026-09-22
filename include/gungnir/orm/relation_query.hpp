8b24388e9b32c1d5ec3b002a5961663db7b13593
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-22 09:55:55 +0400

    fix(relations): keep relation query builder unbound

--- include/gungnir/orm/relation_query.hpp
+++ include/gungnir/orm/relation_query.hpp
@@ -1,35 +1,28 @@
 #pragma once
 
-#include <type_traits>
-#include <utility>
-
 #include <gungnir/model/relation.hpp>
 #include <gungnir/orm/query.hpp>
 
 namespace gungnir::orm {
 
 template <typename Related>
-[[nodiscard]] Query<Related> related_query(const HasOne<Related>& relation) {
-    return Query<Related>{}.where(
-        relation.foreign_key(),
-        model::AttributeValue{}
-    );
+[[nodiscard]] Query<Related> relation_query(const HasOne<Related>&) {
+    return Query<Related>{};
 }
 
 template <typename Related>
-[[nodiscard]] Query<Related> related_query(const HasMany<Related>& relation) {
-    return Query<Related>{}.where(
-        relation.foreign_key(),
-        model::AttributeValue{}
-    );
+[[nodiscard]] Query<Related> relation_query(const HasMany<Related>&) {
+    return Query<Related>{};
 }
 
 template <typename Related>
-[[nodiscard]] Query<Related> related_query(const BelongsTo<Related>& relation) {
-    return Query<Related>{}.where(
-        relation.owner_key(),
-        model::AttributeValue{}
-    );
+[[nodiscard]] Query<Related> relation_query(const BelongsTo<Related>&) {
+    return Query<Related>{};
+}
+
+template <typename Related>
+[[nodiscard]] Query<Related> relation_query(const BelongsToMany<Related>&) {
+    return Query<Related>{};
 }
 
 } // namespace gungnir::orm
